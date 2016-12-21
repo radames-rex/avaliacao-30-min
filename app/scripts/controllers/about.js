80a55188fae42b720d229b11ad0c4fe5927c2f28
radames-rex <radames.rex@gmail.com>
2016-12-21 16:56:20 -0300

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name avaliacao30MinApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the avaliacao30MinApp
+ */
+angular.module('avaliacao30MinApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name avaliacao30MinApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the avaliacao30MinApp
+ */
+angular.module('avaliacao30MinApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
